002080404689d3bdaa8d16af4ed70a47b3effec0
CahangirAsgerov <esgerovcahangir12@gmail.com>
2023-05-29 14:03:06 +0400

    Database structur added

---
+++ assets/js/database.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCoxHTD1biAR9rAfXpWUDemrENVJjfD0ZY",
+  authDomain: "library-store-29c77.firebaseapp.com",
+  projectId: "library-store-29c77",
+  storageBucket: "library-store-29c77.appspot.com",
+  messagingSenderId: "132569894467",
+  appId: "1:132569894467:web:4a368cfa915594d49e8ef6",
+  measurementId: "G-XNSPWLTS56",
+  databaseURL: "https://library-store-29c77-default-rtdb.firebaseio.com/",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+let database = firebase.database();

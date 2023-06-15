2be56e87903740e59acd056b1156627c99362a99
CahangirAsgerov <esgerovcahangir12@gmail.com>
2023-06-16 00:25:25 +0400

    Added functionality to dynamically generate catalog options to homepage

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,49 @@
 $(document).ready(() => {
   getPathName();
 
-  // Home page
-  function fillCatalogSection() {}
+  // Home page and Catalog options books
+  function fillCatalogSection(node) {
+    database.ref(node).on("value", function (snapshot) {
+      localStorage.setItem(
+        "Catalogs",
+        JSON.stringify(Object.values(snapshot.val()))
+      );
+    });
+    let catalogBooks = JSON.parse(localStorage.getItem("Catalogs"));
+    $(".availableCatalogs div.row").html(
+      catalogBooks.map((item) => {
+        return `
+       <div class="col-lg-4 col-md-6 col-12">
+          <div>
+              <p>${item.addTypeValue}</p>
+          </div>
+        </div>
+       `;
+      })
+    );
+
+    // Catalog options books
+    $(".listBooks").html(
+      catalogBooks.map((item) => {
+        return `
+      <li>${item.addTypeValue}</li>
+      `;
+      })
+    );
+  }
+
+  // Click Go to Catalog Button
+  $(".mainGoCatalog button").on("click", function () {
+    console.log("clicked");
+    window.location.pathname = "catalog.html";
+  });
+
+  //  Groups books by catalog
+  function groupBooksByCatalog(node) {
+    database.ref(node).on("value", function (snapshot) {
+      console.log(Object.values(snapshot.val()));
+    });
+  }
 
   // Control Pathname
   function getPathName() {
@@ -15,8 +56,10 @@ $(document).ready(() => {
     });
 
     if (lastPartPathName === "index.html") {
-      fillCatalogSection("");
+      fillCatalogSection("typesBook");
     } else if (lastPartPathName === "catalog.html") {
+      fillCatalogSection("typesBook");
+      groupBooksByCatalog("book");
     } else if (lastPartPathName === "store.html") {
     } else if (lastPartPathName === "contact.html") {
     } else if (lastPartPathName === "search.html") {

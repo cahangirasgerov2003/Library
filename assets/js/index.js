3aeeb2d4410d9fa9bd1dbd9a29c90a35e60aa07f
CahangirAsgerov <esgerovcahangir12@gmail.com>
2023-06-16 23:54:53 +0400

    Slick cards added in catalog page

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,6 @@
 $(document).ready(() => {
   getPathName();
+  let catalogBooks = null;
 
   // Home page and Catalog options books
   function fillCatalogSection(node) {
@@ -8,41 +9,162 @@ $(document).ready(() => {
         "Catalogs",
         JSON.stringify(Object.values(snapshot.val()))
       );
-    });
-    let catalogBooks = JSON.parse(localStorage.getItem("Catalogs"));
-    $(".availableCatalogs div.row").html(
-      catalogBooks.map((item) => {
-        return `
+
+      catalogBooks = JSON.parse(localStorage.getItem("Catalogs"));
+      $(".availableCatalogs div.row").html(
+        catalogBooks.map((item) => {
+          return `
        <div class="col-lg-4 col-md-6 col-12">
           <div>
               <p>${item.addTypeValue}</p>
           </div>
         </div>
        `;
-      })
-    );
+        })
+      );
 
-    // Catalog options books
-    $(".listBooks").html(
-      catalogBooks.map((item) => {
-        return `
+      // Catalog options books
+      $(".listBooks").html(
+        catalogBooks.map((item) => {
+          return `
       <li>${item.addTypeValue}</li>
       `;
-      })
-    );
+        })
+      );
+
+      $(".bookLibrarySection").after(
+        catalogBooks.map(function (item) {
+          return `
+          <section class="typeOfBooksSection">
+          <div class="bestellerTitle d-flex justify-content-center">
+            <h2>${item.addTypeValue}</h2>
+          </div>
+
+          <div class="d-flex justify-content-center">
+            <div class="previousArrow">
+              <img src="./assets/image/icon/previousArrow.svg" alt="arrow" />
+            </div>
+            <div id="${item.addTypeValue}" class="d-flex justify-content-between bookLibraryContainer">
+            </div>
+            <div class="nextArrow">
+              <img src="./assets/image/icon/nextArrow.svg" alt="arrow" />
+            </div>
+          </div>
+        </section>
+          `;
+        })
+      );
+    });
   }
 
   // Click Go to Catalog Button
   $(".mainGoCatalog button").on("click", function () {
-    console.log("clicked");
     window.location.pathname = "catalog.html";
   });
 
   //  Groups books by catalog
   function groupBooksByCatalog(node) {
     database.ref(node).on("value", function (snapshot) {
-      console.log(Object.values(snapshot.val()));
+      localStorage.setItem(
+        "allBooks",
+        JSON.stringify(Object.values(snapshot.val()))
+      );
+
+      playSlickShow(
+        ".bookLibraryContainerAllBooks",
+        JSON.parse(localStorage.getItem("allBooks"))
+      );
+      for (let i = 0; i < catalogBooks.length; i++) {
+        playSlickShow(
+          `#${catalogBooks[i].addTypeValue}`,
+          JSON.parse(localStorage.getItem("allBooks")).filter((item) => {
+            if (item.bookTypeValue === catalogBooks[i].addTypeValue) {
+              return item;
+            }
+          })
+        );
+      }
     });
+  }
+
+  function playSlickShow(section, dataBook) {
+    console.log(dataBook);
+    $(section).html(
+      dataBook.map((item) => {
+        return `
+        <div>
+        <div class="card libraryCards" style="width:184px">
+        <div class="d-flex justify-content-center">
+                <img
+                  src=${item.bookImageUrl1Value}
+                  class="card-img-top"
+                  alt="book"
+                  style="width: 134px; height: 190px; object-fit: cover;"
+                />
+        </div>
+                  <div class="card-body libraryCardsBody">
+                  <h5 class="card-title">${
+                    item.bookNameValue.length > 10
+                      ? item.bookNameValue.slice(0, 11) + "..."
+                      : item.bookNameValue
+                  }</h5>
+                  <p class="card-text">${
+                    item.authorNameValue.length > 10
+                      ? item.authorNameValue.slice(0, 11) + "..."
+                      : item.authorNameValue
+                  }</p>
+                  <button>
+                    <p>READ MORE</p>
+                  </button>
+                </div>
+        </div>
+        </div>
+        `;
+      })
+    );
+    if (dataBook.length > 5) {
+      $(section)
+        .not(".slick-initialized")
+        .slick({
+          infinite: true,
+          autoplay: true,
+          slidesToShow: 5,
+          speed: 700,
+          prevArrow: $(".previousArrow"),
+          nextArrow: $(".nextArrow"),
+        });
+    } else if (dataBook.length <= 5) {
+      $(section).next().addClass("d-none");
+      $(section).prev().addClass("d-none");
+      $(section).removeClass("justify-content-between");
+      $(section).addClass("justify-content-center");
+      $(section).children("div").css({
+        margin: "0 29px",
+      });
+      if (dataBook.length == 0) {
+        $(section).html(`
+          <div>
+            <div class="card libraryCards" style="width:184px">
+              <div class="d-flex justify-content-center">
+                <img
+                  src="./assets/image/Error.png"
+                  class="card-img-top"
+                  alt="book"
+                  style="width: 134px; height: 190px; object-fit: cover;"
+                />
+              </div>
+              <div class="card-body libraryCardsBody">
+                <h5 class="card-title">Error</h5>
+                <p class="card-text">Error</p>
+                <button>
+                  <p>READ MORE</p>
+                </button>
+              </div>
+            </div>
+          </div>
+          `);
+      }
+    }
   }
 
   // Control Pathname
